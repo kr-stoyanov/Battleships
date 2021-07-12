d6fee75fc5179a89e6d3ae970416fb93926c0d20
Krasimir Stoyanov <stoyanovkr@hotmail.com>
2021-07-12 10:56:31 +0300

    Add project files.

---
+++ Battleship/StartUp.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Media;
+
+namespace Battleship
+{
+    public class Program
+    {
+        static int score;
+        static int round;
+        static char[][] map;
+        static char[] rows;
+        static int[] columns;
+        static char[] objects; // [_] blank, [*] mine, [>] ship
+        static List<Coordinates> moves;
+        static Coordinates revealed;
+        static List<Coordinates> coordinates;
+        static string successfulAttack;
+        static string bomb;
+        static string unsuccessfulAttack;
+
+        public static void Main()
+        {
+            Console.Title = "BattleShip";
+
+            rows = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            columns = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            objects = new char[] { '_', '*', '>' };
+
+            map = new char[7][];
+            moves = new List<Coordinates>();
+            coordinates = new List<Coordinates>();
+
+            successfulAttack = @"assets\mixkit-fuel-explosion-1705.wav";
+            bomb = @"assets\mixkit-sea-mine-explosion-1184.wav";
+            unsuccessfulAttack = @"assets\mixkit-jump-into-the-water-1180.wav";
+
+            score = 0;
+            round = 1;
+
+            while (true)
+            {
+                DrawBoard(revealed);
+
+                Console.WriteLine();
+                Console.WriteLine($"Score: {score}");
+                Console.WriteLine($"Round: {round}");
+                Console.WriteLine();
+                Console.Write($"Enter Coordinates to attack(e.g [A, 9]): ");
+
+                string[] userInput = Console.ReadLine()
+                .Split(new char[] { ' ', ',', '-', ';' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.ToUpper())
+                .ToArray();
+
+                if (IsValidInput(userInput))
+                {
+                    revealed = RevealAttackedObject(userInput);
+
+                    if (revealed.Obj == '*')
+                    {
+                        PlaySound(bomb);
+                        DrawBoard(revealed);
+                        Console.WriteLine();
+                        Console.WriteLine("         GAME OVER!");
+                        Thread.Sleep(3000);
+                        GameOver();
+                        break;
+                    }
+                    else if (revealed.Obj == '>')
+                    {
+                        PlaySound(successfulAttack);
+                        score++;
+                    }
+                    else
+                    {
+                        PlaySound(unsuccessfulAttack);
+                    }
+
+                    round++;
+                }
+            }
+        }
+        private static void DrawBoard(Coordinates revealed)
+        {
+            Console.Clear();
+            Console.WriteLine("[==========BATTLESHIP==========]");
+            Console.WriteLine();
+
+
+            Random rnd = new();
+
+            Console.WriteLine($"  {string.Join("  ", columns)}");
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                map[i] = new char[11];
+                Console.Write($"{rows[i]  }");
+
+                for (int j = 1; j < map[i].Length; j++)
+                {
+                    int objIdx = rnd.Next(0, 3);
+                    coordinates.Add(new Coordinates(rows[i], j, objects[objIdx]));
+                    var move = CheckMove(rows[i], j);
+
+                    if (revealed.Obj != '\0' && (revealed.Row == rows[i] && revealed.Col == j))
+                    {
+                        Console.Write($" {revealed.Obj} ");
+                        moves.Add(revealed);
+                    }
+                    else if (move.Obj != '\0')
+                    {
+                        Console.Write($" {move.Obj} ");
+
+                    }
+                    else Console.Write(" . ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static Coordinates RevealAttackedObject(string[] userInput)
+        {
+            var attackedObject = coordinates.FirstOrDefault(x => x.Row == char.Parse(userInput[0]) && x.Col == int.Parse(userInput[1]));
+
+            return attackedObject;
+        }
+
+        private static void GameOver()
+        {
+            string points = score == 1 ? "point" : "points";
+
+            Console.WriteLine();
+            Console.WriteLine($"   You have died in round {round}\n" +
+                              $"         with {score} {points}!");
+            Console.WriteLine("     Good luck next time!");
+            Console.WriteLine();
+        }
+
+        private static bool IsValidInput(string[] userInput)
+        {
+            if (userInput.Length != 2) return false;
+
+            if (!rows.ToList().Contains(char.Parse(userInput[0])) || !columns.ToList().Contains(int.Parse(userInput[1]))) return false;
+
+            return true;
+        }
+
+        private static Coordinates CheckMove(char row, int col)
+        {
+            return moves.FirstOrDefault(x => x.Row == row && x.Col == col);
+        }
+
+        public static void PlaySound(string file)
+        {
+            SoundPlayer player = new();
+            player.SoundLocation = $".\\{file}";
+            player.Play();
+        }
+
+    }
+
+    public struct Coordinates
+    {
+        public Coordinates(char row, int col, char obj)
+        {
+            Row = row;
+            Col = col;
+            Obj = obj;
+        }
+        public char Row { get; set; }
+
+        public int Col { get; set; }
+
+        public char Obj { get; set; }
+    }
+}
